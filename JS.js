0c8654ea3b48e7b73cdbeb8ad12d0f48ec56c5e9
Sramirez2022506 <informatica@alumno.local>
2024-01-16 16:30:52 -0600

    Parte 3

--- JS.js
+++ JS.js
@@ -1,2 +1,21 @@
-const alfabeto
-const 
+const alfabeto = "abcdefghijklmnñopqrstuvwxyz";
+const palabra = [
+    "Cabra",
+    "Gato",
+    "Perro",
+    "Pez",
+    "Elefante"
+];
+const contBotones = document.getElementById("id-contenedor-botones");
+const IntentosMaximos = 5; 
+const PalabrasUsadas = [];
+const BotonReiniciar = document.getElementsByClassName("BotonReiniciar");
+const IntentosRestantes = document.getElementsByClassName("id-intentos-restantes");
+const ContenedorPalabras = document.getElementsByClassName("id-contenedor-palabra");
+
+function generarBotonesLetras() {
+    for (let letra of alfabeto) {
+        const button = document.getElementById("Button");
+        
+    }
+}
